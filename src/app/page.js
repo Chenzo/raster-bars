2ec934536eeff05b034df3ef864569bc963eda20
Chenzo <chenzorama@hotmail.com>
2025-04-06 18:36:23 -0400

    working rasterbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import SimpleRasterBar from "@/components/SimpleRasterBar";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        main
+        <article>
+        <h3>
+          simple raster
+        </h3>
+        <SimpleRasterBar />
+        </article>
       </main>
       <footer className={styles.footer}>
         footer
--- src/components/SimpleRasterBar.js
+++ src/components/SimpleRasterBar.js
@@ -1,28 +1,76 @@
+'use client'
 
-
-//import styles from "./SimpleRasterBar.module.css";
-import c64Colors from "../constants/c64Colors";
+import { useEffect, useRef } from "react";
+import styles from "./SimpleRasterBar.module.css";
+import { c64Colors } from "../constants/c64Colors";
+import { userAgentFromString } from "next/server";
 export default function SimpleRasterBar() {
 
+  const canvasRef = useRef(null);
+  const animationFrameRef = userAgentFromString(null);
 
-    const colorArray = [
-        c64Colors.blue,
-        c64Colors.lightblue,
-        c64Colors.lightgray,
-        c64Colors.lightgreen,
-        c64Colors.white,
-        c64Colors.white,
-        c64Colors.lightgreen,
-        c64Colors.lightgray,
-        c64Colors.lightblue,
-        c64Colors.blue
-    ];
 
+  const colorArray = [
+    c64Colors.blue,
+    c64Colors.lightblue,
+    c64Colors.lightgray,
+    c64Colors.lightgreen,
+    c64Colors.white,
+    c64Colors.white,
+    c64Colors.lightgreen,
+    c64Colors.lightgray,
+    c64Colors.lightblue,
+    c64Colors.blue
+  ];
+
+  const colorArrayRef = useRef([...colorArray]);
+  const frameCounterRef = useRef(0);
+  const speed = 14;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!canvas || !ctx || !colorArrayRef) return;
+    const height = colorArray.length;
+    const width = canvas.parentElement.offsetWidth;
+
+    const draw = () => {
+      frameCounterRef.current++;
+      if (frameCounterRef.current >= speed) {
+
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const colorArray = colorArrayRef.current;
+        colorArray.forEach((color, index) => {
+          ctx.fillStyle = color;
+          ctx.fillRect(0, index, width, 1);
+        });
+
+        // Rotate the array for animation
+        colorArray.push(colorArray.shift());
+        frameCounterRef.current = 0;
+      }
+
+
+      // Request next frame
+      animationFrameRef.current = requestAnimationFrame(draw);
+    }
+
+    draw();
+
+    return () => {
+      if (animationFrameRef.current !== null) {
+        cancelAnimationFrame(animationFrameRef.current);
+      }
+    };
+  }, [colorArray]);
 
   return (
     <section className={styles.rasterbar}>
-      <div className={rasterBarHolder}>
-        rasterbar here
+      <div className={styles.rasterBarHolder}>
+        <canvas ref={canvasRef} />
       </div>
     </section>
   );
